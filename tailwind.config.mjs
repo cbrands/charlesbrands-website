c00f92c3ff769f585b218ff7bddd09101a8c2e45
Charles Brands <charlesbrands@gmail.com>
2024-03-07 20:42:05 +0100

    prapare tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: colors.cyan,
+				secondary: colors.gray,
+				accent: colors.orange
+			},
+		},
+	},
+	plugins: [],
+}
